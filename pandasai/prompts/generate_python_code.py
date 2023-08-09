b50227b7adabebda41f14fd1190f0dff8efe6328
maksim-melaxtech <maksim.egorov@melaxtech.com>
2023-08-09 14:16:47 -0500

    tweaked instructions and column descriptions

--- pandasai/prompts/generate_python_code.py
+++ pandasai/prompts/generate_python_code.py
@@ -24,22 +24,20 @@ You are provided with a pandas dataframe (df) with {num_rows} rows and {num_colu
 This is the metadata of the dataframe:
 {df_head}.
 These are column descriptions:
-
-npi: National Provider Identifier - a unique identification number for healthcare providers.
+npi: National Provider Identifier.
 physician_name: Name of the physician or healthcare provider.
-specialty: The medical specialty or field in which the physician practices.
-physician_phone: Contact phone number for the physician.
-physician_degrees: Academic degrees or qualifications held by the physician.
-physician_disease_tiers: Tiers or categories of diseases the physician specializes in treating.
-physician_existing_site_yes: Indicates whether the physician has an existing medical site ("yes" or "no").
-physician_existing_site_zip3: The zip code prefix of the existing medical site location.
-physician_trialtrove_profile: Presence of a physician's profile on TrialTrove, likely a medical research platform.
-state_full_name: Full name of the state in which the physician practices.
-physician_primary_practice_address: Main address of the physician's primary practice.
+specialty: Medical specialty or field of practice.
+physician_email: Email address of the physician.
+physician_phone: Phone number of the physician.
+physician_degrees: Academic degrees held by the physician.
+physician_disease_tiers: Disease tiers specialized by the physician.
+state_full_name: Full name of the state where the physician practices.
+physician_primary_practice_address: Primary practice address of the physician.
+physician_zip3: Zip code prefix of the physician's practice location.
 primary_organization_affiliation: Main healthcare organization the physician is affiliated with.
-organization_classification: Classification or type of healthcare organization.
-
-num_pt: Number of total patients physician is treating.
+organization_type: Type of healthcare organization.
+organization_classification: Classification of the healthcare organization.
+num_pt: Total number of patients associated with the physician.
 num_pt_Other: Number of patients of other or unspecified racial backgrounds.
 num_pt_White: Number of patients identified as White.
 num_pt_Black: Number of patients identified as Black.
@@ -49,14 +47,12 @@ num_pt_Race_Non_White: Number of patients belonging to non-White racial groups.
 num_pt_Hispanic: Number of patients identified as Hispanic.
 num_pt_Not_Hispanic: Number of patients not identified as Hispanic.
 num_pt_Ethnicity_Unknown: Number of patients with unknown ethnicity information.
-
-num_trials_total: Total number of clinical trials associated with the healthcare provider.
-num_trials_ongoing: Number of ongoing clinical trials associated with the healthcare provider.
-num_trials_past: Number of completed or past clinical trials associated with the healthcare provider.
-
-num_pub_total: Total number of publications associated with a physician or healthcare provider.
-publications_last_5_years: Number of publications associated with a physician or healthcare provider in the last five years.
-physician_pubmed_url: URL or link to the physician's profile or publications on the PubMed database.
+num_trials_total: Total number of clinical trials associated with the physician.
+num_trials_ongoing: Number of ongoing clinical trials associated with the physician.
+num_trials_past: Number of completed or past clinical trials associated with the physician.
+num_pub_total: Total number of publications associated with the physician.
+num_pub_last5yr: Number of publications in the last five years associated with the physician.
+physician_pubmed_url: URL to the physician's profile or publications on PubMed.
 
 When asked about the data, your response should include a python code that describes the dataframe `df`.
 Using the provided dataframe, df, return the python code and make sure to prefix the requested python code with {START_CODE_TAG} exactly and suffix the code with {END_CODE_TAG} exactly to get the answer to the following question:
--- pandasai/prompts/multiple_dataframes.py
+++ pandasai/prompts/multiple_dataframes.py
@@ -13,6 +13,50 @@ class MultipleDataframesPrompt(Prompt):
     text: str = """
 You are provided with the following pandas dataframes:"""
     instruction: str = """
+
+    These are column descriptions for df1:
+npi: National Provider Identifier.
+physician_name: Name of the physician or healthcare provider.
+specialty: Medical specialty or field of practice.
+physician_email: Email address of the physician.
+physician_phone: Phone number of the physician.
+physician_degrees: Academic degrees held by the physician.
+physician_disease_tiers: Disease tiers specialized by the physician.
+state_full_name: Full name of the state where the physician practices.
+physician_primary_practice_address: Primary practice address of the physician.
+physician_zip3: Zip code prefix of the physician's practice location.
+primary_organization_affiliation: Main healthcare organization the physician is affiliated with.
+organization_type: Type of healthcare organization.
+organization_classification: Classification of the healthcare organization.
+num_pt: Total number of patients associated with the physician.
+num_pt_Other: Number of patients of other or unspecified racial backgrounds.
+num_pt_White: Number of patients identified as White.
+num_pt_Black: Number of patients identified as Black.
+num_pt_Asian: Number of patients identified as Asian.
+num_pt_Race_Unknown: Number of patients with unknown racial information.
+num_pt_Race_Non_White: Number of patients belonging to non-White racial groups.
+num_pt_Hispanic: Number of patients identified as Hispanic.
+num_pt_Not_Hispanic: Number of patients not identified as Hispanic.
+num_pt_Ethnicity_Unknown: Number of patients with unknown ethnicity information.
+num_trials_total: Total number of clinical trials associated with the physician.
+num_trials_ongoing: Number of ongoing clinical trials associated with the physician.
+num_trials_past: Number of completed or past clinical trials associated with the physician.
+num_pub_total: Total number of publications associated with the physician.
+num_pub_last5yr: Number of publications in the last five years associated with the physician.
+physician_pubmed_url: URL to the physician's profile or publications on PubMed.
+
+These are column descriptions for df2. Use this dataframe if asked about unique patients:
+physician_zip3: Zip code prefix of a physician's practice location.
+num_pt_zip3: Total number of unique patients associated with the zip code prefix.
+num_pt_Asian_zip3: Count of unique patients with Asian racial background in the zip code prefix.
+num_pt_Black_zip3: Count of unique patients with Black racial background in the zip code prefix.
+num_pt_Other_zip3: Count of unique patients with other or unspecified racial backgrounds in the zip code prefix.
+num_pt_White_zip3: Count of unique patients identified as White in the zip code prefix.
+num_pt_Race_Unknown_zip3: Count of unique patients with unknown racial information in the zip code prefix.
+num_pt_Hispanic_zip3: Count of unique patients identified as Hispanic in the zip code prefix.
+num_pt_Not_Hispanic_zip3: Count of unique patients not identified as Hispanic in the zip code prefix.
+num_pt_Ethnicity_Unknown_zip3: Count of unique patients with unknown ethnicity information in the zip code prefix.
+
 When asked about the data, your response should include a python code that describes the dataframes provided.
 Using the provided dataframes and no other dataframes, return the python code and make sure to prefix the requested 
 python code with {START_CODE_TAG} exactly and suffix the code with {END_CODE_TAG} exactly to get the answer to the following question:
